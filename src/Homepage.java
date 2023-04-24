fb9b0f89b6a5a342c27439939048a4a6e5dcd7d5
DoaaAbdaljbar <doaaabdljbar@gmail.com>
2023-04-24 17:24:12 +0300

    Screen_Shot_Test

---
+++ src/Homepage.java
@@ -0,0 +1,64 @@
+import java.io.IOException;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class Homepage {
+
+		
+		@Test()
+		public void Test_Screen() {
+
+		
+
+
+		WebDriver driver = new EdgeDriver();
+
+		driver.get("http://127.0.0.1:5500/index.html");
+		SoftAssert softassertProcess = new SoftAssert();
+
+//		Date currentDate = new Date();
+//		
+//		String TheAcutalDate = currentDate.toString().replace(":", "-");
+//
+//		TakesScreenshot src = ((TakesScreenshot) driver) ; 
+//		
+//		File SrcFile = src.getScreenshotAs((OutputType.FILE));
+//		
+//		File Dest = new File(".//mypictures/"+TheAcutalDate+".png");
+//		
+//		FileUtils.copyFile (SrcFile,Dest); 
+
+		List<WebElement> thestudents = driver.findElements(By.tagName("option"));
+
+		int theTotalNumberOfStudents = thestudents.size();
+
+		System.out.println(theTotalNumberOfStudents + "this is the orginal number");
+
+		int HowManyItems = 10;
+
+		System.out.println(HowManyItems + "this is the number of items i want remove");
+
+		for (int i = 0; i < HowManyItems; i++) {
+			driver.findElement(By.xpath("//*[@id=\"remove\"]")).click();
+
+		}
+		
+		List <WebElement> thestudentsAfterRemove = driver.findElements(By.tagName("option"));
+		int ActualNumber = thestudentsAfterRemove.size();
+
+		System.out.println("this is the new actaual number"+ActualNumber);
+
+		int expectedITems = theTotalNumberOfStudents - HowManyItems;
+
+		System.out.println(expectedITems + "this is the number i have expected");
+		
+		softassertProcess.assertEquals(ActualNumber, expectedITems);
+		softassertProcess.assertAll();
+
+}}
